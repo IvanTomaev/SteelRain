519fe6cca914745488c5cdf96bbbd8f6ed4afc42
Ivan Tomaev <example@example.com>
2023-03-13 19:09:26 +0300

    C++ is shit

--- quadro_ext/src/decoder.cpp
+++ quadro_ext/src/decoder.cpp
@@ -1,49 +1,42 @@
 #include "rclcpp/rclcpp.hpp"
-#include "quadro_interface/srv/decoder.hpp"   
-
+#include "quadro_interface/srv/binary_decoder.hpp"   
+#include "std_msgs/msg/string.hpp"
 #include <memory>
 #include <string>
 #include <vector>
 
-
-
+using std::placeholders::_1;
+using std::placeholders::_2;
+using std::placeholders::_3;
+using std::string;
 class Decoder : public rclcpp::Node{
 
 
 
 public:
     Decoder()
-    : Node("Decoder"), count_(0)
-    {
-      rclcpp::Service<quadro_interface::srv::Decoderbin>::SharedPtr service =               // CHANGE
-      node->create_service<quadro_interface::srv::Decoderbin>("decoder",  &Decoder::decode);   // CHANGE
-      subscriber_ = this->create_subscriber<std_msgs::msg::String>("key_topic", 10, std::bind(&Decoder::topic_callback, this, _1));
+    : Node("Decoder"){
+         // CHANGE
+      rclcpp::Service<quadro_interface::srv::BinaryDecoder>::SharedPtr service = 
+      this->create_service<quadro_interface::srv::BinaryDecoder>("binary_decoder",  &Decoder::decode);
+      subscriber_ = this->create_subscription<std_msgs::msg::String>("key_topic", 10, std::bind(&Decoder::topic_callback, this, _1));
 
     }
 
   private:
-    std::string key_;
-
-    void decode(std::shared_ptr<quadro_interface::srv::Decoderbin::Request> request,std::shared_ptr<quadro_interface::srv::Decoderbin::Response> response){
-          auto data = request -> encoded;
-          std::byte temp;
-          for(int i = 0; i < data.size()-1; i++){
-              if(i%2 == 0){
-                temp = data[i];
-                data[i] = data[i+1];
-                data[i+1] = temp;
-              }
-              
-              }
+    string key_;
+    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscriber_;
+    size_t count_;
+    void decode(std::shared_ptr<quadro_interface::srv::BinaryDecoder::Request> request,
+    std::shared_ptr<quadro_interface::srv::BinaryDecoder::Response> response){
 
                    }
 
-    void topic_callback(const std_msgs::msg::String & msg) const
+    void topic_callback(const std_msgs::msg::String & msg)
     {
-      key_ = msg.data
+      key_ = msg.data;
     }
-    rclcpp::Subscriber<std_msgs::msg::String>::SharedPtr subscriber_;
-    size_t count_;
+    
 };
 
 
@@ -58,17 +51,12 @@ public:
 
 
 
-void add(const std::shared_ptr<quadro_interface::srv::Decoder::Request> request,     // CHANGE
-          std::shared_ptr<quadro_interface::srv::Decoder::Response>       response)  // CHANGE
-{
-  response->data = request->encoded;                                     // CHANGE
- 
-}
+
 
 int main(int argc, char **argv)
 {
   rclcpp::init(argc, argv);
-
+  
   rclcpp::init(argc, argv);
   rclcpp::spin(std::make_shared<Decoder>());
   rclcpp::shutdown();
